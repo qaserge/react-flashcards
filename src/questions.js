f8c1a3dad621d74f6829e809488bdc632a5947b5
Sergei <qaserge@gmail.com>
2024-01-25 17:59:37 -0500

    added flashcards component with onClick logic and css styles

---
+++ src/questions.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    id: 1,
+    question: "L'affaire est ketchup",
+    answer: `« L'affaire est ketchup » signifie que tout va bien, que tout est parfait, que tout est accompli. Par exemple, votre journée se termine et vous avez effectué l'ensemble des tâches que vous deviez faire. Vous pouvez dire alors que l'affaire est ketchup. Cette expression est aussi utilisée pour mettre un terme à une réunion lorsque le sujet est bouclé. Elle provient d'un slogan publicitaire de sauce tomate, appelée « Heinz », qui a particulièrement marqué les esprits des Québécois. « Avec Heinz, l'affaire est ketchup ! »`,
+  },
+  {
+    id: 2,
+    question: "Tire-toi une bûche",
+    answer: `« Tire-toi une bûche ! », il ne s'agit pas de s'éreinter à tirer un tronc d'arbre, mais simplement de prendre une chaise. Cette expression québécoise trouve son origine pendant la période de la colonisation française. Les conditions de vie des colons étaient précaires, les sièges n'étaient que de simples billots taillés à la hauteur nécessaire. « Tire-toi une bûche ! » signifiait alors « Attrape une bûche pour t'asseoir ! ». L'idiome a survécu jusqu'à nos jours, bien que l'intérieur de la maison soit déjà bien équipé de meubles confortables.`,
+  },
+  {
+    id: 3,
+    question: "Avoir un six pack",
+    answer: `Les canettes de bière sont généralement conditionnées dans des emballages en plastique, présentant deux rangées de 3 bouteilles (packs de 6 bières). L'expression « avoir un six pack » est employée par analogie pour parler des abdominaux parfaitement musclés, similaires à la forme des packs de bières. En France, le terme équivalent est « avoir des tablettes de chocolat ».`,
+  },
+  {
+    id: 4,
+    question: "Avoir la falle basse",
+    answer: `« Avoir la falle basse » exprime un sentiment de lassitude, le fait d'avoir l'air abattu, d'être découragé ou déprimé. Le mot « falle » désigne la poche de l'œsophage des oiseaux. L'expression « avoir la falle basse » signifiait, à l'origine, « avoir faim ». Lorsque quelqu'un a faim, il a rarement un bon moral, c'est pourquoi le sens étendu du terme qui est devenu « être dépité ».`,
+  },
+  {
+    id: 5,
+    question: "Parler à travers son chapeau",
+    answer: `Bien que les Québécois soient de grands défenseurs de la langue française, ils utilisent fréquemment des anglicismes. L'expression « parler à travers son chapeau » est littéralement calquée sur l'anglais « to talk through one's hat ». Elle veut dire parler à tort et à travers, c'est-à-dire parler de quelque chose sans vraiment avoir les connaissances suffisantes, sans grand discernement. Par exemple, « Comment peux-tu critiquer cette fille alors que tu viens à peine de la rencontrer ? Arrête de parler à travers ton chapeau ! »`,
+  },
+  {
+    id: 6,
+    question: "Chanter la pomme",
+    answer: `L'expression « chanter la pomme » signifie « faire la cour à quelqu'un ». Dans la danse traditionnelle québécoise, il existait différentes façons de se tenir la main entre les deux partenaires. L'une des pratiques consistait pour le cavalier à toucher de manière sensuelle la paume de la main de sa cavalière pour la séduire. Avec le temps, le mot « paume » se serait transformé en « pomme » pour donner l'expression « chanter la pomme ».`,
+  },
+  {
+    id: 7,
+    question: "Le boss des bécosses",
+    answer: `« Boss des bécosses » est un nom donné au petit chef détestable, autoritaire et sans envergure. Celui qui profite d'un pouvoir modeste pour s'imposer. C'est une sorte d'insulte. « Bécosse » est un mot français québécois pour désigner les latrines extérieures de qualité médiocre.`,
+  },
+  {
+    id: 8,
+    question: "Avoir un pain au four",
+    answer: `L'expression québécoise « avoir un pain au four » n'a rien de culinaire. C'est une manière bien poétique de parler de grossesse, elle signifie « être enceinte ». Par exemple : Léa a un pain au four, Véronique va être ravie de devenir grand-mère.`,
+  },
+  {
+    id: 9,
+    question: "Avoir une montée de lait",
+    answer: `Au sens propre, « la montée de lait » est utilisée pour parler des femmes qui allaitent. C'est un processus naturel pour produire le lait maternel. Au sens figuré, l'expression « avoir une montée de lait » signifie « être de mauvaise humeur, s'énerver fortement ». Elle tire probablement son origine du lait chauffé qui déborde rapidement de la casserole, et non pas du lait maternel. Au Québec, un homme peut très bien donc avoir une montée de lait !`,
+  },
+];
+
+export default questions;
